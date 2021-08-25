c5affbe952456f86d838411319e21162cc15c57f
Andreas Häberli <andy@pixelmadstudios.com>
2021-08-25 13:14:32 +0200

    added unreal runnable tests

--- Content/Python/test.py
+++ Content/Python/test.py
@@ -1,5 +1,46 @@
+from __future__ import annotations
+import unittest
 import unreal
-import github
+from plugin import Plugin
 
-result = github.get_matching_plugoon_repos()
-unreal.log()
+class Test:
+    success: int = 0
+    fail: int = 0
+    total: int = 0
+    testInstances: list[Test] = []
+
+    def add(self, instance: Test):
+        self.testInstances.append(instance)
+
+    def run(self):
+        unreal.log("------------------------------")
+        unreal.log("Run tests")
+        unreal.log("------------------------------")
+        for instance in self.testInstances:
+            self._run_instance_tests(instance)
+
+    def result(self):
+        unreal.log("------------------------------")
+        unreal.log(f"Tests run: {self.total}")
+        unreal.log(f"Success: {self.success}")
+        unreal.log(f"Fail: {self.fail}")
+        unreal.log("------------------------------")
+
+    def _run_instance_tests(self, instance: Test):
+        testMethods: list[str] = [method for method in dir(instance.__class__) if method.startswith('test_') is True]
+        unreal.log(f"Run tests for {instance.__class__.__name__}")
+        for test in testMethods:
+            self.total += 1
+            try:
+                getattr(instance, test)()
+                self.success += 1
+                unreal.log(f"Success: {instance.__class__.__name__}-> {test}")
+            except:
+                self.fail += 1
+                unreal.log_error(f"  {instance.__class__.__name__}-> {test}")
+        
+test = Test()
+# test.add(MyTest())
+test.run()
+test.result()
+
